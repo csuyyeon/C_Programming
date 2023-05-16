6227cf5c88ad5f6bfe23cf2b72dc89ce3d7a5b9f
csuyyeon <124238983+csuyyeon@users.noreply.github.com>
2023-05-16 17:43:31 +0900

    Rename 1_hello.c to Chapter2/1_hello.c

---
+++ Chapter2/1_hello.c
@@ -0,0 +1,8 @@
+//콘솔 화면에 Hello World! 를 출력하는 프로그램
+#include <stdio.h>                      //헤더파일을 포함
+
+int main(void)                          //메인 함수 시작
+{
+	printf("Hello Wolrd!");             //Hello World! 를 출력
+	return 0;                           //외부로 0값을 반환
+}                                       //메인 함수 종료  
